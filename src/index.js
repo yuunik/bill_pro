8f3559a429fbeb177eb1fc29898d2063a7f34afa
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-11 09:23:57 +0800

    feat: 配置别名路径.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 // 引入 React 扩展库
 import ReactDOM from 'react-dom/client'
 // 引入 App
-import App from './App'
+import App from '@/App'
 
 const root = ReactDOM.createRoot(document.querySelector(".root"))
 root.render(
